631d1fe2100fa450c515a1d5938f209800d75aca
Timothy-D-Filla <timmyfilla@yahoo.com>
2015-02-14 16:07:34 -0600

    Added file manager and sample zip folder
    The zip folder contains the two EDI files we were sent.  However, they
    seem to be .txt files now.  The JavaScript file I added is currently
    just comments, but I will expand it to do these things in the comments.

---
+++ file-manager.js
@@ -0,0 +1,9 @@
+/*Load the zip folder.
+  At this point, it uses a hard-coded zip folder*/
+
+/*Unzip the folder.
+  If another unzipped folder of the same name exists,
+  that folder shall be overwritten*/
+
+/*Go through each file in the unzipped folder*/
+
