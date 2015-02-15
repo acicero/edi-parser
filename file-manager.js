b7b4b3cd076bcecff9ea14250fbc9b44120bcb82
Timothy-D-Filla <timmyfilla@yahoo.com>
2015-02-15 15:22:32 -0600

    Updated file manager
    In theory, it should be able to go through the entries in a zip folder,
    but I can't guarantee for sure.  It stops at the require() function
    call, since that requires the adm-zip plug-in installed.

--- file-manager.js
+++ file-manager.js
@@ -1,9 +1,15 @@
-/*Load the zip folder.
-  At this point, it uses a hard-coded zip folder*/
+var admZip = require('adm-zip');
 
-/*Unzip the folder.
-  If another unzipped folder of the same name exists,
-  that folder shall be overwritten*/
+/*
+ * Load the zip folder.
+ * At this point, it uses a hard-coded zip folder.
+ * The .getEntries() method lets you access the files
+ * without unzipping the folder
+ */
+var zipFolder = new admZip("./SampleData.zip");
+var zipFiles = zipFolder.getEntries();
 
-/*Go through each file in the unzipped folder*/
-
+//Go through each file in the unzipped folder
+zipFiles.forEach(function(zipEntry){
+	console.log(zipEntry.toString());
+});
