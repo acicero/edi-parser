d839fe010a50775c28a2dd03c63afe7bae87e0a4
eric <biggs.eric@gmail.com>
2015-02-23 17:04:45 -0600

    Initial Parser Code

--- parser.js
+++ parser.js
@@ -1,2 +1,26 @@
 /*Edi parser*/
-/* Takes an edi file as agrument to constructor*/
+/* Takes an edi file as argument to constructor*/
+
+
+var file = 'uploads/Fedex850.txt'; // hardcoded file for now
+var length = 0;
+var fieldDelim;
+var subfieldDelim;
+var recordDelim;
+
+fs = require('fs');
+fs.readFile(file, 'utf8', function(err, data){
+	if(err){
+		return console.log(err)
+	}
+	console.log('File has been read successfully.');
+	length = data.length;
+	console.log('File length: ' + length + ' characters');
+	fieldDelim = data.charAt(103);
+	console.log('fieldDelim value: ' + fieldDelim);
+	subfieldDelim = data.charAt(104);
+	console.log('subfieldDelim value: ' + subfieldDelim);
+	recordDelim = data.charAt(105);
+	console.log('recordDelim value: ' + recordDelim);
+	
+});
