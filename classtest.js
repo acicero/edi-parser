b109a0d0b6612207dfab58000e49e25909698ab5
Timothy-D-Filla <timmyfilla@yahoo.com>
2015-03-29 12:08:24 -0500

    File manager now follows testModule's standard
    Also added new code to classtest.js that tests file-manager.js, which
    worked when I tested it.

--- classtest.js
+++ classtest.js
@@ -1,5 +1,20 @@
 var user = require('./testModule.js');
+var fileManager = require('./file-manager.js');
 
 var u = new user("name", "email");
 
 u.f1();
+
+var fm = new fileManager(__dirname + '/SampleData.zip');
+
+//fm.extract();
+console.log(fm.list());
+for(var i = 0; i < fm.numberOfFiles(); i++){
+	console.log("Output for file number " + i + ":\n" + fm.read() + "\n");
+}
+
+if(fm.read() === null){
+	console.log("You read to the end.");
+}else{
+	console.log("You still have files to read.");
+}
